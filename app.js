d9c3d64dbe74ded558483b499e2d28fe2c0ca39d
Johanne <johanne_lewis@hotmail.com>
2019-07-28 01:02:15 +0100

    Use function to initialise variables and initialise new game when new game button is selected

--- app.js
+++ app.js
@@ -11,20 +11,7 @@ GAME RULES:
 
 var scores, roundScores, activePlayer, diceDOM;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-diceDOM = document.querySelector('.dice');
-
-
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 
 //roll dice
@@ -72,6 +59,24 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
         nextPlayer();
      }
 });
+
+// new game button
+document.querySelector('.btn-new').addEventListener('click', init);
+
+function init(){
+    scores = [0,0];
+    activePlayer = 0;
+    roundScore = 0;
+    diceDOM = document.querySelector('.dice');
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('#name-0').textContent = 'Player 1';
+    document.querySelector('#name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.add('active');
+}
 
 function nextPlayer(){
        roundScore = 0;
